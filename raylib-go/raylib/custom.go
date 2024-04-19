68be12aa5a3f08bb47756ad933118bf184a1632c
imprity <imprity041@gmail.com>
2024-04-20 02:55:07 +0900

    adde custom bindings

---
+++ raylib-go/raylib/custom.go
@@ -0,0 +1,22 @@
+package rl
+
+/*
+#include "raylib.h"
+*/
+import "C"
+
+// SwapScreenBuffer - Swap back buffer with front buffer (screen drawing)
+func SwapScreenBuffer(){
+	C.SwapScreenBuffer()
+}
+
+// PollInputEvents - Register all input events
+func PollInputEvents(){
+	C.PollInputEvents()
+}
+
+// WaitTime - Wait for some time (halt program execution)
+func WaitTime(seconds float64){
+	cseconds := (C.double)(seconds)
+	C.WaitTime(cseconds)
+}
