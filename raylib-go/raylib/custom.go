30943157f42a701869d51632be016aefdd9914f1
imprity <imprity041@gmail.com>
2024-05-05 23:26:17 +0900

    added go image to raylib image converter

--- raylib-go/raylib/custom.go
+++ raylib-go/raylib/custom.go
@@ -5,6 +5,12 @@ package rl
 */
 import "C"
 
+import (
+	"image"
+	"image/color"
+	"unsafe"
+)
+
 // SwapScreenBuffer - Swap back buffer with front buffer (screen drawing)
 func SwapScreenBuffer() {
 	C.SwapScreenBuffer()
@@ -19,4 +25,53 @@ func PollInputEvents() {
 func WaitTime(seconds float64) {
 	cseconds := (C.double)(seconds)
 	C.WaitTime(cseconds)
+}
+
+// NewImageFromImage - Returns new Image from Go image.Image
+func NewImageFromImagePro(img image.Image, bgColor Color, alphaMultiply bool) *Image {
+	size := img.Bounds().Size()
+
+	cx := (C.int)(size.X)
+	cy := (C.int)(size.Y)
+	cBgColor := colorCptr(bgColor)
+	ret := C.GenImageColor(cx, cy, *cBgColor)
+
+	for y := 0; y < size.Y; y++ {
+		for x := 0; x < size.X; x++ {
+			imgColor := img.At(x, y)
+
+			rlColor := Color{}
+
+			if alphaMultiply {
+				converted := color.RGBAModel.Convert(imgColor)
+				v := converted.(color.RGBA)
+
+				rlColor = Color{
+					R: v.R,
+					G: v.G,
+					B: v.B,
+					A: v.A,
+				}
+			} else {
+				converted := color.NRGBAModel.Convert(imgColor)
+				v := converted.(color.NRGBA)
+
+				rlColor = Color{
+					R: v.R,
+					G: v.G,
+					B: v.B,
+					A: v.A,
+				}
+			}
+
+			cRlColor := colorCptr(rlColor)
+
+			cx = (C.int)(x)
+			cy = (C.int)(y)
+			C.ImageDrawPixel(&ret, cx, cy, *cRlColor)
+		}
+	}
+
+	v := newImageFromPointer(unsafe.Pointer(&ret))
+	return v
 }
