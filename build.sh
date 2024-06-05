5baca646a752265f123915967b8881015726006f
imprity <imprity041@gmail.com>
2024-06-05 20:01:43 +0900

    improved linux build script

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-go build -tags=noaudio -gcflags="-e"
+if [ "$1" == "" ]; then
+    go build -tags=noaudio -gcflags="-e"
+elif [ "$1" == "debug" ]; then
+	go build -o "fnf-practice-debug" -tags=noaudio -gcflags="-e -l -N" .
+else
+    echo invalid arument "$1"
+fi
+
