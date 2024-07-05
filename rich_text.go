e48a7047544b1c0c56a68e8881233ef1af1b6889
imprity <imprity041@gmail.com>
2024-07-06 08:22:41 +0900

    implemented line break rule

--- rich_text.go
+++ rich_text.go
@@ -1,32 +1,42 @@
 package main
 
 import (
-	rl "github.com/gen2brain/raylib-go/raylib"
 	"fmt"
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"strconv"
+	"unicode/utf8"
 )
 
-var _= fmt.Println
+var _ = fmt.Println
+var _=strconv.Quote
 
 type RichTextStyle struct {
 	FontSize float32
 
-	Font rl.Font
-	SdfFont SdfFont
+	Font       rl.Font
+	SdfFont    SdfFont
 	UseSdfFont bool
 
 	Fill, Stroke FnfColor
-	StrokeWidth float32
+	StrokeWidth  float32
 }
 
 type RichTextElement struct {
-	Text string
-	Pos rl.Vector2
+	Text  string
+	Pos   rl.Vector2
 	Style RichTextStyle
 }
 
-type RichTextFactory struct{
-	Style RichTextStyle
+const (
+	LineBreakChar = iota
+	LineBreakWord
+)
+
+type RichTextFactory struct {
+	Style       RichTextStyle
 	LineSpacing float32
+
+	LineBreakRule int
 
 	width float32
 
@@ -35,24 +45,24 @@ type RichTextFactory struct{
 	elements []RichTextElement
 }
 
-func NewRichTextFactory (width float32) *RichTextFactory{
+func NewRichTextFactory(width float32) *RichTextFactory {
 	return &RichTextFactory{
-		Style : RichTextStyle {
-			FontSize : 30,
-			Font : FontRegular,
-			Fill : Col(0,0,0,255),
+		Style: RichTextStyle{
+			FontSize: 30,
+			Font:     FontRegular,
+			Fill:     Col(0, 0, 0, 255),
 		},
-		LineSpacing : 30,
+		LineSpacing: 30,
 
-		width : width,
+		width: width,
 	}
 }
 
-func (rt *RichTextFactory) Width() float32{
+func (rt *RichTextFactory) Width() float32 {
 	return rt.width
 }
 
-func (rt *RichTextFactory) Print(text string){
+func (rt *RichTextFactory) Print2(text string) {
 	if len(text) <= 0 {
 		return
 	}
@@ -73,49 +83,200 @@ func (rt *RichTextFactory) Print(text string){
 	start := 0
 	savedCursor := rt.cursor
 
-	commit := func(pos int){
+	commit := func(pos int) {
 		rt.elements = append(rt.elements, RichTextElement{
-			Text : text[start:pos],
-			Pos : savedCursor,
-			Style : rt.Style,
+			Text:  text[start:pos],
+			Pos:   savedCursor,
+			Style: rt.Style,
 		})
 		savedCursor = rt.cursor
 		start = pos
 	}
 
-	breakLine := func(){
+	breakLine := func() {
 		rt.cursor.X = 0
 		rt.cursor.Y += rt.LineSpacing
 		savedCursor = rt.cursor
 	}
 
-	for pos, char := range text{
-		if char == '\n'{
+	for pos, char := range text {
+		if char == '\n' {
 			commit(pos)
 			breakLine()
 			start = pos + 1
-		}else{
+		} else {
 			glyph := rl.GetGlyphInfo(font, char)
 
 			charAdvance := float32(glyph.AdvanceX)
-			if charAdvance == 0{
+			if charAdvance == 0 {
 				rec := rl.GetGlyphAtlasRec(font, char)
 				charAdvance = rec.Width
 			}
 			charAdvance *= scaleFactor
 
-			if rt.cursor.X + charAdvance > rt.width{
+			if rt.cursor.X+charAdvance > rt.width {
 				commit(pos)
 				breakLine()
 				start = pos
-			}			
+			}
 
 			rt.cursor.X += charAdvance
 		}
 	}
 
-	if start < len(text){
+	if start < len(text) {
 		commit(len(text))
 	}
 }
 
+func (rt *RichTextFactory) Print(text string) []RichTextElement{
+	if len(text) <= 0 {
+		return []RichTextElement{}
+	}
+	var newElements []RichTextElement
+	// check if cursor is out side width
+
+	if rt.cursor.X > rt.width {
+		rt.cursor.X = 0
+		rt.cursor.Y += rt.LineSpacing
+	}
+
+	font := rt.Style.Font
+	if rt.Style.UseSdfFont {
+		font = rt.Style.SdfFont.Font
+	}
+
+	scaleFactor := rt.Style.FontSize / f32(font.BaseSize)
+
+	getTextSize := func(start, end int) float32{
+		textSize := float32(0)
+		for _, char := range text[start:end] {
+			glyph := rl.GetGlyphInfo(font, char)
+
+			charSize := float32(glyph.AdvanceX)
+			if charSize == 0 {
+				rec := rl.GetGlyphAtlasRec(font, char)
+				charSize = rec.Width
+			}
+			charSize *= scaleFactor
+
+			textSize += charSize
+		}
+		return textSize
+	}
+
+	textStart := 0
+	textEnd := 0
+	textSize := float32(0)
+
+	saveToken := func(tkStart, tkEnd int, tkSize float32){
+		textSize += tkSize
+		textEnd = tkEnd
+	}
+
+	printSavedToken := func() bool{
+		if textEnd > textStart{
+			newElements = append(newElements, RichTextElement{
+				Text:  text[textStart:textEnd],
+				Pos:   rt.cursor,
+				Style: rt.Style,
+			})
+
+			rt.cursor.X += textSize
+			textStart = textEnd
+			textSize = 0
+
+			return true
+		}
+		return false
+	}
+
+	iter := newIteratorForRT([]byte(text), font, rt.LineBreakRule)
+
+	for iter.HasNext(){
+		tkStart, tkEnd := iter.Next()
+
+		if text[tkStart:tkEnd] == "\n"{
+			printSavedToken()
+			rt.cursor.Y += rt.LineSpacing
+			rt.cursor.X = 0
+			textStart = tkEnd
+			textEnd = tkEnd
+		}else{
+			tkSize := getTextSize(tkStart, tkEnd)
+			if rt.cursor.X + textSize + tkSize > rt.width{
+				printSavedToken()
+				if rt.cursor.X > 0 {
+					rt.cursor.Y += rt.LineSpacing
+					rt.cursor.X = 0
+				}
+
+				saveToken(tkStart, tkEnd, tkSize)
+
+				if rt.cursor.X + textSize > rt.width{
+					printSavedToken()
+					rt.cursor.Y += rt.LineSpacing
+					rt.cursor.X = 0
+				}
+			}else{
+				saveToken(tkStart, tkEnd, tkSize)
+				if rt.cursor.X + textSize > rt.width{
+					printSavedToken()
+					rt.cursor.Y += rt.LineSpacing
+					rt.cursor.X = 0
+				}
+			}
+		}
+	}
+
+	printSavedToken()
+
+	return newElements
+}
+
+type iteratorForRT struct {
+	text          []byte
+	lineBreakRule int
+	pos           int
+}
+
+func newIteratorForRT(textAsBytes []byte, font rl.Font, lineBreakRule int) *iteratorForRT {
+	return &iteratorForRT{
+		text:          textAsBytes,
+		lineBreakRule: lineBreakRule,
+	}
+}
+
+func (it *iteratorForRT) HasNext() bool {
+	return it.pos < len(it.text)
+}
+
+func (it *iteratorForRT) Next() (int, int) {
+	if it.lineBreakRule == LineBreakWord {
+		prevPos := it.pos
+
+		for it.pos < len(it.text) {
+			r, s := utf8.DecodeRune(it.text[it.pos:])
+
+			isSpecial := r == '\n' || r == ' '
+
+			if isSpecial {
+				if prevPos == it.pos { // this is the first character we encountered
+					it.pos += s
+				}
+
+				return prevPos, it.pos
+			}
+
+			it.pos += s
+		}
+
+		return prevPos, it.pos
+	} else {
+		prevPos := it.pos
+		_, s := utf8.DecodeRune(it.text[it.pos:])
+		it.pos += s
+
+		return prevPos, it.pos
+	}
+}
--- test_rich.go
+++ test_rich.go
@@ -15,66 +15,101 @@ func init() {
 	})
 }
 
+const _appleText = `An apple is a round, edible fruit produced by an apple tree (Malus spp., among them the domestic or orchard apple; Malus domestica).
+Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found.
+Apples have been grown for thousands of years in Eurasia and were introduced to North America by European colonists.
+Apples have religious and mythological significance in many cultures, including Norse, Greek, and European Christian tradition.`
+
 type RtTestScreen struct {
+	InputId InputGroupId
+
 	elements []RichTextElement
-	factory *RichTextFactory
+
+	fontSize float32
+	lineSpacing float32
+	textWidth float32
+	lineBreakRule int
 }
 
+func (rt *RtTestScreen) GenerateRichTexts() {
+	rt.elements = rt.elements[:0]
+	factory := NewRichTextFactory(rt.textWidth)
+	factory.LineBreakRule = rt.lineBreakRule
+	factory.Style.FontSize = rt.fontSize
+	factory.LineSpacing = rt.lineSpacing
+
+	elements := factory.Print(_appleText)
+
+	for _, e := range elements{
+		rt.elements = append(rt.elements, e)
+	}
+}
 
 func NewRtTestScreen() *RtTestScreen {
 	rt := new(RtTestScreen)
 
-	rt.factory = NewRichTextFactory(100)
+	rt.InputId = NewInputGroupId()
 
-	rt.factory.Print("Hello World! ")
-	rt.factory.Style.Fill = FnfColor{0,255,0,255}
-	rt.factory.Print("This\nIs\nPretty Fucking\n\n\nCool")
-	rt.factory.Style.FontSize *=2
-	rt.factory.LineSpacing *= 2
-	rt.factory.Style.Fill = FnfColor{0,0,255,255}
-	rt.factory.Print("yoyoyoy0")
+	rt.textWidth = 300
+	rt.fontSize = 40
+	rt.lineSpacing = rt.fontSize
+	rt.lineBreakRule = LineBreakChar
 
-
-	rt.elements = rt.factory.elements
+	rt.GenerateRichTexts()
 
 	return rt
 }
 
 func (rt *RtTestScreen) Update(deltaTime time.Duration) {
+	repeat := time.Millisecond * 100
+
+	changed := false
+
+	if HandleKeyRepeat(rt.InputId, repeat, repeat, rl.KeyRight){
+		rt.textWidth += 10
+		changed = true
+	}
+	if HandleKeyRepeat(rt.InputId, repeat, repeat, rl.KeyLeft){
+		rt.textWidth -= 10
+		changed = true
+	}
+
+	if changed{
+		rt.GenerateRichTexts()
+	}
 }
 
 func (rt *RtTestScreen) Draw() {
-	rl.ClearBackground(ToRlColor(Col01(1,1,1,1)))
+	rl.ClearBackground(ToRlColor(Col01(1, 1, 1, 1)))
 
 	rl.DrawLine(
-		i32(rt.factory.Width()), 0, i32(rt.factory.Width()), SCREEN_HEIGHT,
-		ToRlColor(Col01(1,0,0,1)),
+		i32(rt.textWidth), 0, i32(rt.textWidth), SCREEN_HEIGHT,
+		ToRlColor(Col01(1, 0, 0, 1)),
 	)
 
 	{
-		offsetY := rt.factory.LineSpacing
-		for offsetY < SCREEN_HEIGHT{
+		offsetY := rt.lineSpacing
+		for offsetY < SCREEN_HEIGHT {
 			rl.DrawLine(
 				0, i32(offsetY), SCREEN_WIDTH, i32(offsetY),
-				ToRlColor(Col01(1,0,0,1)),
+				ToRlColor(Col01(1, 0, 0, 1)),
 			)
 
-			offsetY += rt.factory.LineSpacing
+			offsetY += rt.lineSpacing
 		}
 	}
 
-
-	for _, e := range rt.elements{
-		if e.Style.UseSdfFont{
+	for _, e := range rt.elements {
+		if e.Style.UseSdfFont {
 			if e.Style.StrokeWidth <= 0 {
 				DrawTextSdf(e.Style.SdfFont, e.Text,
 					e.Pos, e.Style.FontSize, 0, ToRlColor(e.Style.Fill))
-			}else{
+			} else {
 				DrawTextSdfOutlined(e.Style.SdfFont, e.Text,
 					e.Pos, e.Style.FontSize, 0,
 					ToRlColor(e.Style.Fill), ToRlColor(e.Style.Stroke), e.Style.StrokeWidth)
 			}
-		}else{
+		} else {
 			rl.DrawTextEx(e.Style.Font, e.Text,
 				e.Pos, e.Style.FontSize, 0, ToRlColor(e.Style.Fill))
 		}
