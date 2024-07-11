fb5a74d54d9344fb4b8ec9d926023c523a1b6251
imprity <imprity041@gmail.com>
2024-07-11 19:56:44 +0900

    updated linux build file

--- build.sh
+++ build.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
 if [ "$1" == "" ]; then
-    go build -tags=noaudio -gcflags="-e"
+
+	go build -o "fnf-practice" -tags=noaudio -gcflags="-e" main.go
+
 elif [ "$1" == "debug" ]; then
-	go build -o "fnf-practice-debug" -tags=noaudio -gcflags="-e -l -N" .
+
+	go build -o "fnf-practice-debug" -tags=noaudio -gcflags="-e -l -N" main.go
+
+elif [ "$1" == "font-gen" ]; then
+
+	go build -o "font-gen" -tags=noaudio -gcflags="-e" font_gen.go
+
+elif [ "$1" == "font-gen-debug" ]; then
+
+	go build -o "font-gen-debug" -tags=noaudio -gcflags="-e -l -N" font_gen.go
+
+elif [ "$1" == "all" ]; then
+
+	go build -o "fnf-practice" -tags=noaudio -gcflags="-e" main.go
+	go build -o "font-gen" -tags=noaudio -gcflags="-e" font_gen.go
+
 else
+
     echo invalid arument "$1"
+
 fi
 
