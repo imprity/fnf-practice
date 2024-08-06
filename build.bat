a39bf4b67e1bee78b879f76966b7e8b77ace3e9e
imprity <imprity041@gmail.com>
2024-08-06 15:20:23 +0900

    now release includes hit sound

--- build.bat
+++ build.bat
@@ -30,6 +30,9 @@ if "%1" == "" (
 	set "to_build=fnf-practice.exe"
 	set "build_source=main.go"
 	call :build_command
+	if !errorlevel! neq 0 exit /b !errorlevel!
+
+	echo "BUILD SUCCESS"
 	goto :quit
 )
 
@@ -37,6 +40,9 @@ if "%1"=="font-gen" (
 	set "to_build=font-gen.exe"
 	set "build_source=font_gen.go"
 	call :build_command
+	if !errorlevel! neq 0 exit /b !errorlevel!
+
+	echo "BUILD SUCCESS"
 	goto :quit
 )
 
@@ -44,6 +50,9 @@ if "%1"=="debug" (
 	set "to_build=fnf-practice-debug.exe"
 	set "build_source=main.go"
 	call :build_debug_command
+	if !errorlevel! neq 0 exit /b !errorlevel!
+
+	echo "BUILD SUCCESS"
 	goto :quit
 )
 
@@ -51,6 +60,10 @@ if "%1"=="font-gen-debug" (
 	set "to_build=font-gen-debug.exe"
 	set "build_source=font_gen.go"
 	call :build_debug_command
+
+	if !errorlevel! neq 0 exit /b !errorlevel!
+
+	echo "BUILD SUCCESS"
 	goto :quit
 )
 
@@ -63,6 +76,7 @@ if "%1"=="release" (
 	call :create_release
 	if !errorlevel! neq 0 exit /b !errorlevel!
 
+	echo "BUILD SUCCESS"
 	goto :quit
 )
 
@@ -80,6 +94,7 @@ if "%1"=="all" (
 	call :create_release
 	if !errorlevel! neq 0 exit /b !errorlevel!
 
+	echo "BUILD SUCCESS"
 	goto :quit
 )
 
@@ -111,8 +126,13 @@ goto :quit
 	set "release_folder=fnf-practice-win64-v%git_ver_str%"
 
 	xcopy /Y .\fnf-practice.exe ".\release\%release_folder%\"
+	if !errorlevel! neq 0 exit /b !errorlevel!
+
+	xcopy /Y .\assets\hit-sound.ogg ".\release\%release_folder%\"
+	if !errorlevel! neq 0 exit /b !errorlevel!
 
 	mkdir .\release\zip
+	if !errorlevel! neq 0 exit /b !errorlevel!
 
 	powershell Compress-Archive -Force^
 		".\release\%release_folder%"^
