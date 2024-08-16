be4a04986df6b1aa8e499a68a7db357d1770405f
imprity <imprity041@gmail.com>
2024-08-17 03:27:14 +0900

    updated build script for linux

--- build.sh
+++ build.sh
@@ -16,6 +16,17 @@ build_debug_func () {
 	go build -o "$1" -tags=noaudio -gcflags=all="-e -l -N" "$2"
 }
 
+build_demo_func () {
+	echo "building demo"
+	echo "to_build : fnf-practice-demo"
+	echo "build_source : main.go"
+
+    go build -o "fnf-practice-demo" \
+        -tags=noaudio,demoreplay \
+        -gcflags=all="-e" \
+        "main.go"
+}
+
 git describe --tags --always --abbrev=0 > /dev/null
 if [ $? -eq 0 ]; then
     git describe --tags --always --abbrev=0 > git_tag.txt
@@ -31,7 +42,11 @@ if [ "$1" == "" ]; then
 
 elif [ "$1" == "clean" ]; then
 
-    rm -f fnf-practice fnf-practice-debug font-gen font-gen-debug
+    rm -f fnf-practice \
+        fnf-practice-debug \
+        font-gen \
+        font-gen-debug \
+        fnf-practice-demo
 
 elif [ "$1" == "debug" ]; then
 
@@ -45,10 +60,15 @@ elif [ "$1" == "font-gen-debug" ]; then
 
     build_debug_func font-gen-debug font_gen.go
 
+elif [ "$1" == "demo" ]; then
+
+    build_demo_func
+
 elif [ "$1" == "all" ]; then
 
     build_func fnf-practice main.go
     build_func font-gen font_gen.go
+    build_demo_func
 
 else
 
